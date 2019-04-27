d569f78c4b3311b03b90b97fc4b680b7ac2e95d9
HumanGreed <33626233+HumanGreed@users.noreply.github.com>
2019-04-27 11:25:44 +0200

    + Added Unity Project - Removed Unreal Project

---
+++ LudumDareracing/Assets/scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject car;
+	public float x_car;
+	public float y_car;
+	public float z_car;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		// access rotation of the car
+		x_car = car.transform.eulerAngles.x;
+		y_car = car.transform.eulerAngles.y;
+		z_car = car.transform.eulerAngles.z;
+
+		transform.eulerAngles = new Vector3(x_car - x_car, y_car, z_car - z_car);
+	}
+}
